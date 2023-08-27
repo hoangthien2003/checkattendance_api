a2dfb1eda0c518192d690607435bf63e489c930b
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-08-27 14:50:46 +0700

    init project

---
+++ src/main/java/com/iot102/checkattendance_api/CheckattendanceApiApplication.java
@@ -0,0 +1,12 @@
+package com.iot102.checkattendance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckattendanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckattendanceApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/iot102/checkattendance_api/CheckattendanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot102.checkattendance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckattendanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

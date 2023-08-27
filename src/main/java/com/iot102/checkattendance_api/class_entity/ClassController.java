03118ebf5990d3a2657fcf30a73c724f52ed14e0
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-08-28 01:19:01 +0700

    change to push on server

--- src/main/java/com/iot102/checkattendance_api/class_entity/ClassController.java
+++ src/main/java/com/iot102/checkattendance_api/class_entity/ClassController.java
@@ -3,23 +3,17 @@ package com.iot102.checkattendance_api.class_entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.boot.configurationprocessor.json.JSONArray;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.http.HttpStatus;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iot102.checkattendance_api.student.StudentDTO;
 import com.iot102.checkattendance_api.study.StudyService;
-
-import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 @RequestMapping("/api/class")
@@ -30,26 +24,26 @@ public class ClassController {
         this.studyService = studyService;
     }
 
-    JSONObject setJson(String studentID, String fingerprintID) throws JSONException {
+    JSONObject setJson(String studentID, String fingerprintID) {
         JSONObject json = new JSONObject();
-        json.put("studentID", studentID);
-        json.put("fingerprintID", fingerprintID);
+        try {
+            json.put("studentID", studentID);
+            json.put("fingerprintID", fingerprintID);
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return json;
     }
 
     @GetMapping("/{classID}/students")
     @ResponseBody
-    public ResponseEntity<?> getStudentsByClassID(@PathVariable String classID) throws JsonProcessingException {
+    public ResponseEntity<String> getStudentsByClassID(@PathVariable String classID) {
         List<StudentDTO> studentList = new ArrayList<StudentDTO>();
         studentList = studyService.getStudentsByClassID(classID);
         JSONArray jsonArray = new JSONArray();
         studentList.forEach((item) -> {
-            try {
-                jsonArray.put(setJson(item.getStudentID(), item.getFingerprintID()));
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            jsonArray.put(setJson(item.getStudentID(), item.getFingerprintID()));
         });
         return ResponseEntity.ok(jsonArray.toString());
     }

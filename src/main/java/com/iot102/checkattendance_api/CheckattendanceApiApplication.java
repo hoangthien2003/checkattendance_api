40a5de9733b40437b477b72714464e5bab50b2b2
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-08-31 23:45:01 +0700

    change db address

--- src/main/java/com/iot102/checkattendance_api/CheckattendanceApiApplication.java
+++ src/main/java/com/iot102/checkattendance_api/CheckattendanceApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CheckattendanceApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CheckattendanceApiApplication.class, args);
 	}
